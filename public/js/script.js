07357a82c519e2c1e42048f13026e0a4091dba63
Jimmy Lin <usmcjimmy@gmail.com>
2016-07-17 00:53:02 -0400

    functioning login-auth using firebase

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,74 @@
 $(function(){
+
   console.log("running script file")
 
   $('#myModal').on('shown.bs.modal', function(){
     $('#myInput').focus()
   })
 
+  $('#loginSubmit').click(function(){
+    const userName = $('#userName').val();
+    const userPassword = $('#userPassword').val();
+
+    toggleSignIn();
+    $('#loginButton').hide()
+    $('#logoutButton').show()
+
+    $('#myModal').modal('toggle');
+    return false;
+
+
+  })
+
+  $('#logoutButton').click(function(){
+
+    toggleSignOut();
+
+    $('#logoutButton').hide()
+    $('#loginButton').show()
+
+  })
+
+
+  // fire base SignIn
+  function toggleSignIn(){
+    if (firebase.auth().currentUser){
+      firebase.auth().signOut();
+    } else {
+      const email = $('#userName').val();
+      const password = $('#userPassword').val();
+
+      if(email.length < 2) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if(password.length < 2) {
+        alert('Please enter a password');
+        return;
+      }
+
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        if(errorCode === 'auth/wrong-password') {
+          alert('Wrong password');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        console.log("Did NOT Logined")
+      });
+    }
+    console.log("Logined")
+
+  }
+
+  // fire base Logout
+  function toggleSignOut(){
+    firebase.auth().signOut()
+  }
 
 
 })
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const firebase = require('firebase');
 
 /* app setting */
 const port = process.env.PORT || 3000;
